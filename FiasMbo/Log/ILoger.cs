95fa23f663cfb2497bacf2beaf5825b796f97aa9
ALNO <a-novik0ff@mail.ru>
2019-08-15 14:27:16 +0300

    add log

---
+++ FiasMbo/Log/ILoger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAS.Log
+{
+    interface ILoger
+    {
+        void Log(string Message);
+    }
+}
---
+++ FiasMbo/Log/Loger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FIAS.Log
+{
+    public class Loger : ILoger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
